da125101430ea8e93cd31e9c7990fbae4b7dd236
tptonporpongsakornkaewdam668-cmd <tp.tonpor.pongsakorn.kaewdam668@gmail.com>
2026-03-24 20:06:21 +0700

    Fix: Imported updateBalance service to Admin Routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,6 +6,7 @@ const { adminLoginRules } = require("../middleware/validate");
 const { adminLimiter } = require("../middleware/rateLimiter");
 const { db } = require("../db");
 const { invalidate } = require("../services/cache");
+const { updateBalance } = require("../services/wallet");
 const { v4: uuidv4 } = require("uuid");
 const multer = require("multer");
 
